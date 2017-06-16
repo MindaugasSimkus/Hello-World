45b0968fc1c78380b43571da2f6e11118c65bace
MindaugasSimkus <shimkus.mindaugas@gmail.com>
2017-06-16 20:14:46 +0300

    start of javascript coding

---
+++ js/script.js
@@ -0,0 +1,14 @@
+function teisingai() {
+    document.getElementById('klausimas').innerHTML = '<h1>Kiek bus du plius du? <br> Atsakymas teisingas</h1>';
+
+}
+function neteisingai() {
+    document.getElementById('klausimas').innerHTML = '<h1>Kiek bus du plius du? <br> Atsakymas neteisingas</h1>';
+    alert("Išmok skaičiuot");
+}
+function teisingai2() {
+   console.log("Buvo atsakyta teisingai.")
+}
+function neteisingai2() {
+    console.log("Buvo atsakyta neteisingai.")
+}
