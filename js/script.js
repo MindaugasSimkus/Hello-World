7e1ee400aa7e313cd8c7a654ac3ac7e2a5fe77c8
MindaugasSimkus <shimkus.mindaugas@gmail.com>
2017-06-17 19:18:47 +0300

    table cell color change

--- js/script.js
+++ js/script.js
@@ -12,3 +12,16 @@ function teisingai2() {
 function neteisingai2() {
     console.log("Buvo atsakyta neteisingai.");
 }
+function rand(){
+	document.getElementsByTagName("td").style.backgroundColor = "red";
+	var tabledata = ['first-cell', 'second-cell', 'third-cell', 'fourth-cell', 'fifth-cell', 'sixth-cell', 'seventh-cell', 'eighth-cell', 'ninth-cell'];
+	var y = Math.floor(Math.random() * 9);
+	document.getElementById("atsitiktinis_skaicius").innerHTML ='Atsitiktinis langelis: <br/>' + tabledata[y]; 
+	document.getElementById(tabledata[y]).style.backgroundColor = "green";
+}
+function raid() {
+          var xyz = 0;
+          var somearray = ['a','b','c'];
+          var content = somearray[xyz];
+          document.getElementById('raide').innerHTML = 'gauname tokia raide:' + somearray[xyz];
+        }
