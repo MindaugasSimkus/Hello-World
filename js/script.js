87dafe634b57ae34935411098bbc73e529489e99
MindaugasSimkus <shimkus.mindaugas@gmail.com>
2017-06-17 22:34:30 +0300

    nearing the end of homework

--- js/script.js
+++ js/script.js
@@ -11,17 +11,46 @@ function teisingai2() {
 }
 function neteisingai2() {
     console.log("Buvo atsakyta neteisingai.");
-}
-function rand(){
-	document.getElementsByTagName("td").style.backgroundColor = "red";
+} // HOMEWORK FUNCTIONS
+function rand(){ // function for painting random cell of the table
+	white();
 	var tabledata = ['first-cell', 'second-cell', 'third-cell', 'fourth-cell', 'fifth-cell', 'sixth-cell', 'seventh-cell', 'eighth-cell', 'ninth-cell'];
 	var y = Math.floor(Math.random() * 9);
-	document.getElementById("atsitiktinis_skaicius").innerHTML ='Atsitiktinis langelis: <br/>' + tabledata[y]; 
+	document.getElementById('atsitiktinis_skaicius').innerHTML ='Atsitiktinis langelis: <br/>' + tabledata[y]; 
 	document.getElementById(tabledata[y]).style.backgroundColor = "green";
 }
-function raid() {
-          var xyz = 0;
-          var somearray = ['a','b','c'];
-          var content = somearray[xyz];
-          document.getElementById('raide').innerHTML = 'gauname tokia raide:' + somearray[xyz];
-        }
+function white() { // paints all cells in white
+	var tabledata = ['first-cell', 'second-cell', 'third-cell', 'fourth-cell', 'fifth-cell', 'sixth-cell', 'seventh-cell', 'eighth-cell', 'ninth-cell'];
+	for (var i = 0; i <= tabledata.length-1; i++) {
+		document.getElementById(tabledata[i]).style.backgroundColor = "white";
+	}
+}
+function findgreencell() {
+	var tabledata = ['first-cell', 'second-cell', 'third-cell', 'fourth-cell', 'fifth-cell', 'sixth-cell', 'seventh-cell', 'eighth-cell', 'ninth-cell'];
+	var i=0;
+	document.getElementById(tabledata[i]).style.backgroundColor;
+	while (document.getElementById(tabledata[i]).style.backgroundColor == 'white') {
+		i++;
+	}
+	var green_cell_index = i;
+	document.getElementById('zaliacele').innerHTML ='Zalio langelio numeris: ' + green_cell_index;
+	return green_cell_index;
+}
+function sequence() {
+	var tabledata = ['first-cell', 'second-cell', 'third-cell', 'fourth-cell', 'fifth-cell', 'sixth-cell', 'seventh-cell', 'eighth-cell', 'ninth-cell'];
+	var i = findgreencell();
+
+	document.getElementById('spalva').innerHTML ='Zalio langelio numeris: ' + i;
+	if (i == 0) {
+		document.getElementById(tabledata[++i]).style.backgroundColor = "green";		
+	} else {
+		if (i <= 8) {
+			document.getElementById(tabledata[i]).style.backgroundColor = "white";
+			document.getElementById(tabledata[++i]).style.backgroundColor = "green";
+		} else if (i == 0) {
+			document.getElementById(tabledata[i]).style.backgroundColor = "green";
+		} else {
+			i = 0;
+		}
+	}
+}
